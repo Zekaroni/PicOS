553366ebde37a61b5f9f5f66f1761e9a17806bc2
Zekaroni <ezekiel.hall.04@gmail.com>
2026-09-23 13:31:18 -0400

    started working on a graphics lib

---
+++ video/GraphicsLibrary.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "RenderEngine.h"
+
+class GraphicsLibrary
+{
+    // TODO: Add triangle to render engine
+    /** Graphic Library Main Notes
+     * Have a main overaching GUI interface
+     * - Grids
+     * - - 
+     * - Containing local boxes
+     * - - Think containers tkinter
+     * - - Everything is in relative space
+     * - 
+     */
+private:
+    RenderEngine renderEngine;
+public:
+    GraphicsLibrary()
+    {
+        
+    }
+};
