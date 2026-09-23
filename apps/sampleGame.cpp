74ca0bdee24412ffb957f1bc5137cde9a442686b
Zekaroni <ezekiel.hall.04@gmail.com>
2026-09-23 13:21:25 -0400

    renammed GAME to APP for exporting

--- apps/sampleGame.cpp
+++ apps/sampleGame.cpp
@@ -1,12 +1,12 @@
 #include "../system/SystemAPI.h"
 
 #if defined(_WIN32)
-    #define GAME_EXPORT __declspec(dllexport)
+    #define APP_EXPORT __declspec(dllexport)
 #elif defined(__linux__) || defined(__APPLE__)
-    #define GAME_EXPORT __attribute__((visibility("default")))
+    #define APP_EXPORT __attribute__((visibility("default")))
 #else
     // Fallback for standalone Pico 2 OS
-    #define GAME_EXPORT
+    #define APP_EXPORT
 #endif
 
 int playerX;
@@ -16,7 +16,7 @@ int speed;
 
 extern "C" 
 {
-    GAME_EXPORT void application_init(SystemAPI* api) 
+    APP_EXPORT void application_init(SystemAPI* api) 
     {
         playerX    = 50;
         playerY    = 50;
@@ -24,7 +24,7 @@ extern "C"
         speed      = 1;
     }
 
-    GAME_EXPORT void application_update(SystemAPI* api) 
+    APP_EXPORT void application_update(SystemAPI* api) 
     {
         if (api->input->isPressed(Button::RIGHT)) playerX+=speed;
         if (api->input->isPressed(Button::LEFT))  playerX-=speed;
